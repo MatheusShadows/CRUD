1c50b1d80958ff7d0e1364f94727555ae698b94d
Matheus Arnaud Pina Baker <matheus_baker@hotmail.com>
2021-07-09 15:00:06 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import React, {useEffect, useState} from 'react';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
 import api from './services/api';
 import styled from 'styled-components';
 import card from './App.module.css';
@@ -6,124 +10,160 @@ import './styles/globals.css'
 import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 import DiretoLogo from './images/img-1.png';
-import { left } from '@popperjs/core';
+
 interface IContrato{
-  dtr_id: number;
-  dtr_nomedevedor: string;
-  dtr_estadodevedor: string;
-  dtr_status: boolean;
-  dtr_cidadedevedor: string;
+  id: number;
+  nomedevedor: string;
+  empresa: string;
+  statuscontrato: boolean;
+  numerocontrato: number;
 }
 
 const App: React.FC = () => {
   const [displayBasic, setDisplayBasic] = useState(false); 
-
+  const [displayAtualizar, setDisplayAtualizar] = useState(false); 
+  const [numeroAtt, setNumeroAtt] = useState(0);
   const onClick = (name: string) => {
       setDisplayBasic(true);
   }
   const onHide = (name: string) => {
       setDisplayBasic(false);
   }
-  const renderFooter = (name: string) => {
-      return (
-          <div>
-              <Button style={{marginLeft:"90%",color:"red"}} label="Close" onClick={() => onHide(name)} />
-          </div>
-      );
-  }
+  const onClickA = (name: string) => {
+    setDisplayAtualizar(true);
+}
+const onHideA = (name: string) => {
+    setDisplayAtualizar(false);
+}
+
   const [contratosLista, setContratosLista] = useState<IContrato[]>([{
-    dtr_id: 1,
-    dtr_nomedevedor: 'Jeferson Silva',
-    dtr_estadodevedor: 'PA',
-    dtr_status: true,
-    dtr_cidadedevedor: 'Belém',
+    id: 1,
+    nomedevedor: 'Jeferson Silva',
+    empresa: 'PA',
+    statuscontrato: true,
+    numerocontrato: 1
   },{
-    dtr_id: 2,
-    dtr_nomedevedor: 'Mateus Nascimento',
-    dtr_estadodevedor: 'PA',
-    dtr_status: false,
-    dtr_cidadedevedor: 'Belém',
+    id: 2,
+    nomedevedor: 'Mateus Nascimento',
+    empresa: 'PA',
+    statuscontrato: false,
+    numerocontrato: 2
   },{
-    dtr_id: 3,
-    dtr_nomedevedor: 'Jaozin Leite',
-    dtr_estadodevedor: 'PA',
-    dtr_status: false,
-    dtr_cidadedevedor: 'Belém',
+    id: 3,
+    nomedevedor: 'Jaozin Leite',
+    empresa: 'PA',
+    statuscontrato: false,
+    numerocontrato: 3
   }, {
-    dtr_id: 4,
-    dtr_nomedevedor: 'Isaac Asimov',
-    dtr_estadodevedor: 'AM',
-    dtr_status: true,
-    dtr_cidadedevedor: 'Manaus',
+    id: 4,
+    nomedevedor: 'Isaac Asimov',
+    empresa: 'AM',
+    statuscontrato: true,
+    numerocontrato: 4
   },{
-    dtr_id: 5,
-    dtr_nomedevedor: 'Franz Kafka',
-    dtr_estadodevedor: 'AP',
-    dtr_status: true,
-    dtr_cidadedevedor: 'Macapá',
+    id: 5,
+    nomedevedor: 'Franz Kafka',
+    empresa: 'AP',
+    statuscontrato: true,
+    numerocontrato: 5
   }  ]);
   const [nameinput,setNameinput] = useState('')
-  const param = ["dtr_nomedevedor"]
-//  useEffect(()=>{
-//    async function getData(){
-//      try {
-//        const { data } = await api.get('/dadosProtesto/ListaTitulos')
-//        setContratosLista(data.DadosListTitulos);
-//     } catch (err) {
-//        console.log(err);
-//      }
-//   }
-//    getData()
-//  })
+ useEffect(()=>{
+   async function getData(){
+     try {
+       const { data } = await api.get('/retorno')
+       console.log("axios get",data);
+       setContratosLista(data[0]);
+       console.log("axios get",contratosLista);
+     } catch (err) {
+       console.log(err);
+     }
+   }
+   getData()
+  }, [])
+  function Status(estado:boolean) {
   
+    if (estado == true) {
+      return 'Vigente';
+    }else{
+      return 'Não Vigente';
+    }
+  }
   function search(dados: IContrato[]) {
    return dados.filter((contrato)=>{
-     return (contrato.dtr_nomedevedor.indexOf(nameinput.trim())>-1)
+     return (contrato.nomedevedor.indexOf(nameinput.trim())>-1)
    })
+  }
+  function Excluir(id: number){
+    console.log(id);
+    //  return api.delete('', id);
+  }
+  function NC(id: number){
+    setNumeroAtt(id)
   }
 
   return (
     <div >
-      <img src={DiretoLogo} style={{margin:"0 25% 0 25%",width:"50%"}}/>
-      
-            <Button label="Criar Novo Contrato" icon="pi pi-external-link" onClick={() => onClick('displayBasic')} />
+      <img src={DiretoLogo} style={{margin:"0 25% 0 25%",width:"50%",clear:"both"}}/>
       <div>
-            <Dialog className={card.card} visible={displayBasic} style={{ width: '50vw' }} header={renderFooter('displayBasic')} onHide={() => onHide('displayBasic')}>
+            <Dialog className={card.card} visible={displayBasic} style={{ width: '50vw' }} header="Criar Novo Contrato" onHide={() => onHide('displayBasic')}>
         <div >
-        <form>
-                <h2>Criar novo contrato</h2>
-          <label form = "dtr_nomedevedor">Nome: </label><br/>
+        <form className="p-field" action="192.168.1.9:3038" method="post">
+          <label form = "nomedevedor">Nome: </label><br/>
           <input
           type = "text" 
-          id = "dtr_nomedevedor" 
-          name = "dtr_nomedevedor"  
+          id = "nomedevedor" 
+          name = "nomedevedor"  
           required
           placeholder = "Digite o nome do devedor"></input><br/>
-          <label form = "dtr_estadodevedor" >Estado: </label><br/>
+          <label form = "empresa" >Empresa: </label><br/>
           <input 
           type = "text" 
-          id = "dtr_estadodevedor" 
-          name = "dtr_estadodevedor"  
+          id = "empresa" 
+          name = "empresa"  
           required
-          placeholder = "Digite estado"></input><br/>
-          <label form = "dtr_cidadedevedor">Cidade: </label><br/>
-          <input 
-          type = "text" id = "dtr_cidadedevedor" 
-          name = "dtr_cidadedevedor"  
-          required
-          placeholder = "Digite a cidade"></input><br/>
-          <input type = "radio" id = "true" value = "true" name="dtr_status" defaultChecked></input>
+          placeholder = "Digite o nome da empresa"></input><br/>
+          <input type = "radio" id = "true" value = "true" name="statuscontrato" defaultChecked></input>
           <label form = "true">Vigente</label><br/>
-          <input type = "radio" id = "false" value = "false" name="dtr_status"></input>
+          <input type = "radio" id = "false" value = "false" name="statuscontrato"></input>
           <label form = "false">Não Vigente</label><br/>
-          <input type = "submit" value = "ENVIAR" className={card.submit}></input>
+          <input type = "submit" value = "ENVIAR" className={card.submit} ></input>
         </form>
           </div>
             </Dialog>
       </div>
+      <Dialog className={card.card} visible={displayAtualizar} style={{ width: '50vw' }} header="Atualizar Contrato" onHide={() => onHideA('displayAtualizar')}>
+        <div >
+        <form className="p-field" action="192.168.1.9:3038" method="put">
+          <h2>Contrato N° {numeroAtt}</h2>
+          <label form = "nomedevedor">Nome: </label><br/>
+          <input
+          type = "text" 
+          id = "nomedevedor" 
+          name = "nomedevedor"  
+          required
+          placeholder = "Digite o nome do devedor"></input><br/>
+          <label form = "empresa" >Empresa: </label><br/>
+          <input 
+          type = "text" 
+          id = "empresa" 
+          name = "empresa"  
+          required
+          placeholder = "Digite o nome da empresa"></input><br/>
+          <input type = "radio" id = "true" value = "true" name="statuscontrato" defaultChecked></input>
+          <label form = "true">Vigente</label><br/>
+          <input type = "radio" id = "false" value = "false" name="statuscontrato"></input>
+          <label form = "false">Não Vigente</label><br/>
+          <input type = "submit" value = "ENVIAR" className={card.submit} ></input>
+        </form>
+          </div>
+            </Dialog>
+          <h1>Contratos</h1>
+      <ul>
       <label htmlFor="search-form">
-            <span>Digite seu nome aqui</span><br/>
+            <h2>Buscar Contrato</h2><br/>
               <input
+              style={{width:"50%", height:"30px", fontSize:"18px",marginBottom:"30px"}}
               type="search"
               name="search-form"
               id="search-form"
@@ -132,26 +172,24 @@ const App: React.FC = () => {
               onChange={(e)=>setNameinput(e.target.value)}
               />
           </label>
-          <h1>Contratos</h1>
-      <ul>
+          <Button label="Criar Novo Contrato" style={{marginLeft: "33%",marginBottom:"20px"}}  onClick={() => onClick('displayBasic')} />
         <table >
-        <div >
         <tr >
     <th >Nome</th>
     <th >Status</th> 
-    <th >Estado</th>
-    <th >ID</th>
+    <th >Empresa</th>
+    <th >Numero Contrato</th>
         </tr>
           {search(contratosLista).map(nomes=>(  
-        <tr key={nomes.dtr_id}>
-    <th >{nomes.dtr_nomedevedor}</th>
-    <th >{nomes.dtr_status}</th> 
-    <th >{nomes.dtr_estadodevedor}</th>
-    <th >{nomes.dtr_id}</th>
-    <th ><button style={{color:"red"}}>Excluir</button></th>
+        <tr key={nomes.id}>
+    <td >{nomes.nomedevedor}</td>
+    <td >{Status(nomes.statuscontrato)}</td> 
+    <td >{nomes.empresa}</td>
+    <td >{nomes.numerocontrato}</td>
+    <td ><button onClick={()=>Excluir(nomes.id)}>Excluir</button></td>
+    <td ><button className={card.update} onClick={() => {onClickA('displayAtualizar');NC(nomes.numerocontrato)}}>Atualizar</button></td>
         </tr>
           ))}
-        </div>
         </table>
       </ul>
     </div>
